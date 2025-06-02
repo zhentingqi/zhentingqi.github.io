fdc6c74bcbf5b6bbe98bc938b7b2e9a57737b9e9
zhentingqi <140472590+zhentingqi@users.noreply.github.com>
2025-06-02 00:46:20 +0000

    Deploying to gh-pages from @ zhentingqi/zhentingqi.github.io@7b94f6fa8ab7297b68acd27763e66371fd69d0c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,13 +511,6 @@ ninja.data = [{
           window.open("https://github.com/zhentingqi", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-instagram',
         title: 'Instagram',
         section: 'Socials',
@@ -530,13 +523,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/zhentingqi", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
