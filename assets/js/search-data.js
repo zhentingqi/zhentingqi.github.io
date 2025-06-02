2e6f14bcc58e718b7c65f87cb66e49f561a231fc
zhentingqi <140472590+zhentingqi@users.noreply.github.com>
2025-06-02 02:53:00 +0000

    Deploying to gh-pages from @ zhentingqi/zhentingqi.github.io@333f1232b8a8a735f26263aa72a63d7e832fb60b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-will-be-joining-google-deepmind-mountain-view-office-as-a-student-researcher",
+          title: 'Will be joining Google DeepMind (Mountain View office) as a Student Researcher!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
